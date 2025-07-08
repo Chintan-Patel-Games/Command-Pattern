10924bce91ee05d34b9f89239bcff5c72bba16ca
Chintan Patel <chintanpatelit024@gmail.com>
2025-07-08 16:51:34 +0530

    Added: Create Replay Service

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
-using System.Collections.Generic;
 using Command.Utilities;
 using Command.Sound;
+using System.Collections.Generic;
 using Command.Input;
 using Command.Player;
 using Command.UI;
 using Command.Events;
 using Command.Battle;
 using Command.Actions;
+using UnityEngine.UI;
 using Command.Commands;
+using Command.Replay;
 
 namespace Command.Main
 {
@@ -29,6 +31,7 @@ namespace Command.Main
         public BattleService BattleService { get; private set; }
         public PlayerService PlayerService { get; private set; }
         public CommandInvoker CommandInvoker { get; private set; }
+        public ReplayService ReplayService { get; private set; }
 
         [SerializeField] private UIService uiService;
         public UIService UIService => uiService;
@@ -47,10 +50,11 @@ namespace Command.Main
             EventService = new EventService();
             ActionService = new ActionService();
             InputService = new InputService();
+            CommandInvoker = new CommandInvoker();
             BattleService = new BattleService(battleScriptableObjects);
             PlayerService = new PlayerService();
             uiService.Init(battleScriptableObjects.Count);
-            CommandInvoker = new CommandInvoker();
+            ReplayService = new ReplayService();
         }
 
         private void Update() => InputService.UpdateInputService();
